0c7e8776b48e96bfe4c0a885ab45389b79cb8a33
SeanBecker <techtronixwebdesign@gmail.com>
2019-02-25 21:05:37 -0600

    updated: corrected eslint settings for use with react native :sparkles:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,19 @@
 module.exports = {
-  'extends': 'airbnb',
+  'extends': [
+    'eslint:recommended', 
+    'plugin:react/recommended'
+   ],
   'parser': 'babel-eslint',
   'env': {
     'jest': true,
   },
   'rules': {
-    'no-use-before-define': 'off',
+    'no-use-before-define': 'on',
     'react/jsx-filename-extension': 'off',
-    'react/prop-types': 'off',
+    'react/prop-types': 'on',
     'comma-dangle': 'off'
   },
   'globals': {
-    "fetch": false
+    "fetch": true
   }
 }
