801c2fcbb87309cb9ac42fd46dc23dad54b8e39b
SeanBecker <techtronixwebdesign@gmail.com>
2019-03-19 18:45:39 -0500

    update: fetching contrib data from github json, flatmap with listItem init design

--- src/components/contributors/index.js
+++ src/components/contributors/index.js
@@ -8,8 +8,6 @@ const styles = StyleSheet.create({
   content: {
     display: 'flex',
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   }
 });
 
@@ -22,46 +20,13 @@ export default class ContributorsView extends Component {
     };
   }
 
-
-  // getData() {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //     .then(response => response.json)
-  //     .then((data) => {
-  //       this.setState({
-  //         dataLoaded: true,
-  //         contributorsData: JSON.parse(data)
-  //       });
-  //       console.log(this.state)
-  //     });
-  // }
-
-  // asyncGetData() {
-  //   fetch('https://github.com')
-  //     .then(response => response.json)
-  //     .then((data) => {
-  //       this.setState({
-  //         contributorsData: JSON.parse(data)
-  //       });
-  //       console.log(this.state);
-  //     });
-  // }
-
   render() {
-    if (this.state.dataLoaded) {
-      return (
-        <View>
-          <PageHeader />
-          <View style={styles.content}>
-            <ListContainer />
-          </View>
-        </View>
-      );
-    }
     return (
-      <View>
+      <View style={styles.content}>
         <PageHeader />
         <View style={styles.content}>
-          <Text>Data goes here</Text>
+          { this.state.dataLoaded
+            ? <ListContainer style={styles.content}/> : <Text>No Data Found</Text> }
         </View>
       </View>
     );
--- src/components/contributors/listContainer/index.js
+++ src/components/contributors/listContainer/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React, { Component } from 'react';
 import {
   StyleSheet,
@@ -5,12 +6,31 @@ import {
   Text,
   FlatList
 } from 'react-native';
-import { List, ListItem } from 'native-base';
+import {
+  ListItem,
+  Left,
+  Body,
+  Thumbnail
+} from 'native-base';
 
 const style = StyleSheet.create({
   flex: {
     display: 'flex',
-    flex: 1
+    flex: 1,
+  },
+  listItem: {
+    height: 100,
+  },
+  name: {
+    fontSize: 20
+  },
+  teamIcon: {
+    width: 40,
+    height: 40,
+    marginRight: 10,
+    borderRadius: 20,
+    borderColor: '#212121',
+    borderWidth: 1
   }
 });
 
@@ -24,29 +44,78 @@ export default class ListContainer extends Component {
           id: '0',
           name: 'Sean RabbitWerks',
           avatar: 'https://yt3.ggpht.com/-hRIruU8PArM/AAAAAAAAAAI/AAAAAAAAAAA/6YE3uj59cgA/s88-c-k-no-mo-rj-c0xffffff/photo.jpg',
-          developmentTeams: ['Planning', 'Frontend', 'Backend', 'Design']
+          developmentTeams: ['Planning', 'Frontend', 'Backend', 'Design', 'DevOps', 'Testing']
         },
         {
           id: '1',
           name: 'Joshua Ferris',
-          avatar: '',
+          avatar: 'https://avatars3.githubusercontent.com/u/22730186?s=460&v=4',
           developmentTeams: ['Planning', 'Frontend', 'Backend', 'Design']
         },
-      ]
+      ],
+      githubData: [],
     };
   }
 
+  componentDidMount() {
+    fetch('https://app-backend-iel278lgr.now.sh/contributors')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          githubData: data,
+        }, () => {
+          console.log('data fetched');
+          console.log(this.state.githubData);
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
   render() {
-    if (this.state.contributors) {
+    if (this.state.githubData) {
       return (
-        <List style={style.flex}>
-          <FlatList
-            style={style.flex}
-            data={this.state.contributors}
-            renderItem={({ item }) => <Text>{item.name}</Text>}
-            keyExtractor={item => item.id}
-          />
-        </List>
+        <FlatList
+          style={style.flex}
+          data={this.state.githubData}
+          renderItem={({ item }) => {
+            console.log(item);
+            const {
+              name,
+              teamIds,
+              joined,
+              github,
+              image,
+              active
+            } = item;
+
+            return (
+            <ListItem style={style.listItem} avatar>
+              <Left>
+                <Thumbnail source={{ uri: image }} />
+              </Left>
+              <Body>
+                <Text style={style.name}>
+                  {name}
+                </Text>
+                <View style={{ backgroundColor: '#ccc', height: 60 }}>
+                  <Text>Development Teams</Text>
+                  <View style={{ display: 'flex', flexDirection: 'row', flex: 1 }}>
+                    {
+                      teamIds.map((team, index) => <View
+                      key={index}
+                      style={style.teamIcon}>
+                        { console.log(team) }
+                      </View>)
+                    }
+                  </View>
+                </View>
+              </Body>
+            </ListItem>
+            );
+          }
+        }
+          keyExtractor={item => item.name}
+        />
       );
     }
     return <Text>No data found</Text>;
